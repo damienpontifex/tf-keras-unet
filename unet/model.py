4af2f2f55d26a34c29c863097d3445e8d9ae879d
Damien Pontifex <damien.pontifex@gmail.com>
2019-01-29 21:17:59 +0000

    initial work on using deconvolution instead of resize

--- unet/model.py
+++ unet/model.py
@@ -13,10 +13,9 @@ def _conv_block(inputs: tf.Tensor, filters: int, repeat=2) -> tf.Tensor:
 def _up_conv(inputs: tf.Tensor) -> tf.Tensor:
     """Deconvolution"""
     input_shape = inputs.get_shape().as_list()
-    size = [2*input_shape[1], 2*input_shape[2]]
-    # Try nearest neighbour resize method 
-    # https://medium.com/towards-data-science/autoencoders-introduction-and-implementation-3f40483b0a85
-    return tf.image.resize_images(inputs, size=size)
+    # Match deconvolution filters with input last dimension
+    filters = input_shape[-1]
+    return l.Conv2DTranspose(filters, kernel_size=2, padding='same', activation='relu')(inputs)
 
 
 def unet_model(features, labels, mode, params):
@@ -24,29 +23,29 @@ def unet_model(features, labels, mode, params):
 
     # Provide sample of original image to tensorboard
     tf.summary.image('inputs', features['image'])
-    
+
     # Even though we have two classes we are actually only predicting one
     # The other is given by its abscence and hence binary classification
     NUM_CLASSES = 1
-    
+
     if labels is not None:
         # Sample of our truth label for tensorboard
         tf.summary.image('labels', labels)
 
     # Use the image as the input or our current head of the model
     net = features['image']
-    
+
     # Used to store layers that are copied to the up-path of the unet model
     copy_layers = []
     # Define the number of filters in each stage of the up and down path
-    filter_counts = [65, 128, 256, 512]
+    filter_counts = [64, 128, 256, 512]
 
     # Contractive path
     for num_filters in filter_counts:
         net = _conv_block(net, num_filters)
         copy_layers.append(net)
         net = l.MaxPool2D(pool_size=2, strides=2, padding='same')(net)
-    
+
     # Apply final conv that doesn't have output copied and no maxpool
     net = _conv_block(net, 2 * filter_counts[-1])
 
--- unet/train.py
+++ unet/train.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Unet segmentation model training entrypoint"""
 
 import os
 from argparse import ArgumentParser
@@ -7,24 +8,17 @@ import tensorflow as tf
 import model
 import data
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument('--data-dir', help='Location where TFRecord files are')
-    parser.add_argument('--model-dir', help='Location where model will be saved')
-    parser.add_argument('--max-steps', default=10000)
-
-    args, _ = parser.parse_known_args()
+def train(data_dir, model_dir, max_steps):
 
     # Ensure our model directory exists
-    tf.gfile.MakeDirs(args.model_dir)
-
+    tf.gfile.MakeDirs(model_dir)
 
     config = tf.estimator.RunConfig(
         train_distribute=tf.contrib.distribute.MirroredStrategy())
 
     estimator = tf.estimator.Estimator(
-        model_fn=model.unet_model, 
-        model_dir=args.model_dir,
+        model_fn=model.unet_model,
+        model_dir=model_dir,
         config=config,
         params={
             'learning_rate': 1e-4
@@ -32,14 +26,20 @@ def main():
     )
 
     tf.logging.set_verbosity(tf.logging.INFO)
-    
-    train_input_fn = lambda: data.make_dataset(os.path.join(args.data_dir, 'train-*.tfrecord'), batch_size=2, shuffle=True)
-    train_spec = tf.estimator.TrainSpec(train_input_fn, max_steps=args.max_steps) #, hooks=[debug_hook])
 
-    val_input_fn = lambda: data.make_dataset(os.path.join(args.data_dir, 'validation-0.tfrecord'), batch_size=2, shuffle=False)
+    train_input_fn = lambda: data.make_dataset(os.path.join(data_dir, 'train-*.tfrecord'), batch_size=2, shuffle=True)
+    train_spec = tf.estimator.TrainSpec(train_input_fn, max_steps=max_steps) #, hooks=[debug_hook])
+
+    val_input_fn = lambda: data.make_dataset(os.path.join(data_dir, 'validation-0.tfrecord'), batch_size=2, shuffle=False)
     eval_spec = tf.estimator.EvalSpec(val_input_fn, steps=20)
 
     tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('--data-dir', help='Location where TFRecord files are')
+    parser.add_argument('--model-dir', help='Location where model will be saved')
+    parser.add_argument('--max-steps', default=10000)
+
+    args, _ = parser.parse_known_args()
+    train(**vars(args))
